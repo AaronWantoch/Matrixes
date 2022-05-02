19dd96ec207d7e2ffc7f80030282dde0a93e389d
AaronWantoch <58564314+AaronWantoch@users.noreply.github.com>
2022-05-02 10:15:38 +0200

    gauss

--- MatrixCalculator.py
+++ MatrixCalculator.py
@@ -1,49 +1,71 @@
 import math
+import numpy as np
 from copy import deepcopy
 
 class MatrixCalculator:
     @staticmethod
-    def calculatJacobi(A, b):
+    def calculateJacobi(A, b):
         iterations=0
         U = MatrixCalculator.getUpperMatrix(A)
         L = MatrixCalculator.getLowerMatrix(A)
         D = MatrixCalculator.getDiagonalMatrix(A)
         inversedD = MatrixCalculator.inverseDiagonal(D)
+        minusInversedD = MatrixCalculator.multiplyMatrixByNumber(inversedD, -1)
+        LUsum = MatrixCalculator.sumMatrixes(L, U)
 
-        factor1 = MatrixCalculator.multiplyMatrixByMatrix(inversedD, L+U)
+        factor1 = MatrixCalculator.multiplyMatrixByMatrix(
+            minusInversedD,
+            LUsum)
         factor2 = MatrixCalculator.multiplyMatrixByVector(inversedD, b)
 
+        minusb = MatrixCalculator.multiplyVectorByNumber(b, -1)
         r = [1]*len(A)
-        res = MatrixCalculator.multiplyMatrixByVector(A, r) + -1*b
+
+        res = MatrixCalculator.sumVectors(
+            MatrixCalculator.multiplyMatrixByVector(A, r),
+            minusb) #Ar-b
         norm = MatrixCalculator.vectorNorm(res)
+
         while norm > math.pow(10, -9):
-            r = MatrixCalculator.multiplyMatrixByVector(factor1, r) + factor2
-            res = MatrixCalculator.multiplyMatrixByVector(A, r) + -1*b
+            r = MatrixCalculator.sumVectors(
+                MatrixCalculator.multiplyMatrixByVector(factor1, r),
+                factor2)
+            res = MatrixCalculator.sumVectors(
+                  MatrixCalculator.multiplyMatrixByVector(A, r),
+                  minusb)
             norm = MatrixCalculator.vectorNorm(res)
+            iterations += 1
+        return r, iterations
 
-            iterations+=1
-        return  r, iterations
-
+    @staticmethod
     def calculateGaussSeidle(A, b):
-        iterations=0
+        iterations = 0
         U = MatrixCalculator.getUpperMatrix(A)
         L = MatrixCalculator.getLowerMatrix(A)
         D = MatrixCalculator.getDiagonalMatrix(A)
-        inversedD = MatrixCalculator.inverseDiagonal(D)
+        inversedDLSum = MatrixCalculator.inverse(MatrixCalculator.sumMatrixes(D, L))
 
-        factor1 = MatrixCalculator.multiplyMatrixByMatrix(inversedD, L+U)
-        factor2 = MatrixCalculator.multiplyMatrixByVector(inversedD, b)
+        factor1 = MatrixCalculator.multiplyMatrixByNumber(inversedDLSum, -1)
+        factor2 = MatrixCalculator.multiplyMatrixByVector(inversedDLSum, b)
 
-        r = [1]*len(A)
-        res = MatrixCalculator.multiplyMatrixByVector(A, r) + -1*b
+        minusb = MatrixCalculator.multiplyVectorByNumber(b, -1)
+        r = [1] * len(A)
+
+        res = MatrixCalculator.sumVectors(
+            MatrixCalculator.multiplyMatrixByVector(A, r),
+            minusb)  # Ar-b
         norm = MatrixCalculator.vectorNorm(res)
-        while norm > math.pow(10, -9):
-            r = MatrixCalculator.multiplyMatrixByVector(factor1, r) + factor2
-            res = MatrixCalculator.multiplyMatrixByVector(A, r) + -1*b
-            norm = MatrixCalculator.vectorNorm(res)
 
-            iterations+=1
-        return  r, iterations
+        while norm > math.pow(10, -9):
+            r = MatrixCalculator.sumVectors(
+                MatrixCalculator.multiplyMatrixByVector(factor1, MatrixCalculator.multiplyMatrixByVector(U, r)),
+                factor2)
+            res = MatrixCalculator.sumVectors(
+                MatrixCalculator.multiplyMatrixByVector(A, r),
+                minusb)
+            norm = MatrixCalculator.vectorNorm(res)
+            iterations += 1
+        return r, iterations
 
 
 
@@ -78,33 +100,34 @@ class MatrixCalculator:
     @staticmethod
     def eliminate(r1, r2, col, target=0):
         factor = (r2[col] - target) / r1[col]
+        if factor == 0.0:
+            return
         for i in range(len(r2)):
             r2[i] -= factor * r1[i]
 
     @staticmethod
     def gaussElimination(A):
         for i in range(len(A)):
-            #If 0 is on diagonal switch rows
+            # If 0 is on diagonal switch rows
             if A[i][i] == 0:
-                for j in range(i + 1, len(A)):
-                    if A[i][j] != 0:
+                for j in range(i + 1, min(i + 3, len(A))): #only check 2 next cause rest are 0
+                    if A[j][i] != 0:
                         A[i], A[j] = A[j], A[i]
                         break
 
-            for j in range(i + 1, len(A)):
+            for j in range(i + 1, min(i + 3, len(A))):#only eliminate 2 next cause rest are 0
                 MatrixCalculator.eliminate(A[i], A[j], i)
         for i in range(len(A) - 1, -1, -1):
-            for j in range(i - 1, -1, -1):
+            for j in range(i - 1, max(i - 3, -1), -1):
                 MatrixCalculator.eliminate(A[i], A[j], i)
         for i in range(len(A)):
-            MatrixCalculator.eliminate(A[i], A[i], i, 1) #Set the left ones to one
+            MatrixCalculator.eliminate(A[i], A[i], i, 1)  # Set the diagonal ones to one
         return A
 
     @staticmethod
     def inverse(A):
         tmp = [[] for _ in A]
         for i, row in enumerate(A):
-            assert len(row) == len(A)
             tmp[i].extend(row + [0] * i + [1] + [0] * (len(A) - i - 1))
         MatrixCalculator.gaussElimination(tmp)
         ret = []
@@ -120,13 +143,29 @@ class MatrixCalculator:
         return ret
 
     @staticmethod
+    def inverseSymethric(A):
+        ret = deepcopy(A)
+        for i in range(len(A)):
+            ret[i][i] = 1 / ret[i][i]
+        return ret
+
+    @staticmethod
     def multiplyMatrixByMatrix(A,B):
         ret = []
         for i in range(len(A)):
-            row=A[i][:]
-            column=[i[0] for i in B]
+            current=[]
+            row = A[i]
+            for j in range(len(B)):
+                column = [k[j] for k in B]
+                current.append(MatrixCalculator.multiplyVectorByVector(row, column))
+            ret.append(current)
+        return ret
 
-            ret.append(MatrixCalculator.multiplyVectorByVector(row, column))
+    @staticmethod
+    def multiplyVectorByVector(a, b):
+        ret = 0
+        for i in range(len(a)):
+            ret += a[i]*b[i]
         return ret
 
     @staticmethod
@@ -135,15 +174,8 @@ class MatrixCalculator:
         for i in range(len(A)):
             current = 0
             for j in range(len(A[i])):
-                current += A[i][j]*b[i]
+                current += A[j][i]*b[i]
             ret.append(current)
-        return ret
-
-    @staticmethod
-    def multiplyVectorByVector(a, b):
-        ret = []
-        for i in range(len(a)):
-            ret.append(a[i]*b[i])
         return ret
 
     @staticmethod
@@ -153,3 +185,40 @@ class MatrixCalculator:
             ret += math.pow(a[i], 2)
         return math.sqrt(ret)
 
+    @staticmethod
+    def sumMatrixes(A, B):
+        ret = []
+        for i in range(len(A)):
+            row = []
+            for j in range(len(B)):
+                row.append(A[i][j]+B[i][j])
+            ret.append(row)
+        return ret
+
+    @staticmethod
+    def sumVectors(A, B):
+        ret = []
+        for i in range(len(A)):
+            ret.append(A[i] + B[i])
+        return ret
+
+    @staticmethod
+    def multiplyVectorByNumber(a, x):
+        return [element * x for element in a]
+
+    @staticmethod
+    def multiplyMatrixByNumber(A, x):
+        ret = []
+        for i in range(len(A)):
+            ret.append([element * x for element in A[i]])
+        return ret
+
+    @staticmethod
+    def forwardSubstitiution(A, b):
+        ret = []
+        for i in range(len(A)):
+            sum=0
+            for j in range(i):
+                sum+=A[i][j] * ret[j]
+            ret.append((b[i]-sum)/A[i][i])
+        return ret
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from MatrixCalculator import MatrixCalculator
 if __name__ == '__main__':
     c = 9
     d = 6
-    N=9*c*d
-    A, b = DataGenerator.generateA(N,2, 4)
-    _, iterations = MatrixCalculator.calculateJacobi(A, b)
-    print("Using Jacob method it was solved in", iterations, "iterations")
+    N = 9*c*d
+    A, b = DataGenerator.generateA(N, 2, 4)
+    #_, iterationsJacobi = MatrixCalculator.calculateJacobi(A, b)
+    #print("Using Jacobi method it was solved in", iterationsJacobi, "iterations")
+    _, iterationsGaussSeidle = MatrixCalculator.calculateGaussSeidle(A, b)
+    print("Using Gauss-Seidle method it was solved in", iterationsGaussSeidle, "iterations")
